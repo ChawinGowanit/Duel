c10f59b579d7c748bfd5aa6f832e75559d3fc6a8
Chaiwat Chaiyapuekcharoen <eik.gauss1234@gmail.com>
2020-05-17 15:59:50 +0700

    push la

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -9,8 +9,8 @@ import logic.Turn;
 
 public class GameController {
 	private static int phase;
-	public static InitialCardDeck initialCardDeck = new InitialCardDeck();
-	public static InitialPlaceCard placeCardDeck = new InitialPlaceCard();
+	public static final InitialCardDeck INITIALCARDDECK = new InitialCardDeck();
+	public static final InitialPlaceCard PLACECARDDECK = new InitialPlaceCard();
 	public static Player player1 = new Player(1);
 	public static Player player2 = new Player(2);
 	public static AttackBoard ATKboard = new AttackBoard();
@@ -19,16 +19,16 @@ public class GameController {
 	public static boolean endGame = false;
 
 	public static void initializeGame() {
-		GameController.mainBoard.addCardOnBoard(GameController.initialCardDeck, 1);
-		GameController.mainBoard.addCardOnBoard(GameController.initialCardDeck, 2);
-		GameController.mainBoard.addCardOnBoard(GameController.initialCardDeck, 3);
+		GameController.mainBoard.addCardOnBoard(GameController.INITIALCARDDECK, 1);
+		GameController.mainBoard.addCardOnBoard(GameController.INITIALCARDDECK, 2);
+		GameController.mainBoard.addCardOnBoard(GameController.INITIALCARDDECK, 3);
 		GameController.mainBoard.firstUpdateCardOnBoard(1);
 		GameController.mainBoard.firstUpdateCardOnBoard(2);
 		GameController.mainBoard.firstUpdateCardOnBoard(3);
 		GameController.mainBoard.updateCardOnBoard(1);
 		GameController.mainBoard.updateCardOnBoard(2);
 		GameController.mainBoard.updateCardOnBoard(3);
-		GameController.placeBoard.addCardOnBoard(GameController.placeCardDeck.placeCardDeck);
+		GameController.placeBoard.addCardOnBoard(GameController.PLACECARDDECK.placeCardDeck);
 		GameController.setPhase(1);
 	}
 
